f439e70f2735acb1f752261ab4662a0750b9ed14
kevinsims1 <kevindsims1@gmail.com>
2020-09-29 19:31:50 -0500

    added images to projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,10 @@ import SEO from "../components/seo"
 import { FaGithubSquare, FaEnvelope, FaLaptop, FaGithub} from "react-icons/fa"
 import { IconContext } from "react-icons";
 import "../components/index.css"
+import gfs from '../img/grailsforsell.png'
+import weather from '../img/weather.png'
+import donor from '../img/donor.png'
+import utube from '../img/utube.png'
 
 const IndexPage = () => {
   return (
@@ -16,7 +20,7 @@ const IndexPage = () => {
           <p className="main-p">Full Stack Web Developer focused on making dope products through new technologies. I like building anything JavaScript related. Outside of tech I love basketball, fashion and video games. I'm interested in opportunities that deal with new and exciting technologies.</p>
           <IconContext.Provider value={{ size: "32px", className: "global-class-name" }}>
           <div className="icons">
-             <a href="https://github.com/kevinsims1" className="ghIcon"><FaGithubSquare /></a>
+             <a href="https://github.com/phatstraw" className="ghIcon"><FaGithubSquare /></a>
               <a href="mailto:Kevindsims1@gmail.com" className="ghIcon"><FaEnvelope /></a>
           </div>
           </IconContext.Provider>
@@ -32,66 +36,70 @@ const IndexPage = () => {
         </div>
         <div className="projects">
           <div className="item">
-            <h4>
-              JavaScript
-            </h4>
+            <div className='item-wrap'>
             <h2>
               Grails For Sell
             </h2>
+            <img src={gfs} />
+            </div>
+            
             <p>
              Fully functional e-commerce site. Includes a cart, add items for admin users, as well as signup/signin.
             </p>
             <div className="icons">
               <a href="https://ksks2121.netlify.app/" className="ghIcon"><FaLaptop /></a>
-              <a href="https://github.com/kevinsims1/grailsforsell" className="ghIcon"><FaGithub /></a>
+              <a href="https://github.com/phatstraw/grailsforsell" className="ghIcon"><FaGithub /></a>
             </div>
           </div>
 
           <div className="item">
-            <h4>
-              JavaScript
-            </h4>
+            <div className='item-wrap'>
             <h2>
               Youtube Clone
             </h2>
+            <img src={utube} />
+            </div>
+           
             <p>
               Search for your favorite videos on Youtube, using this "Youtube Clone".
             </p>
             <div className="icons">
-              <a href="https://kevinsims1.github.io/u-tube/" className="ghIcon"><FaLaptop /></a>
-              <a href="https://github.com/kevinsims1/u-tube" className="ghIcon"><FaGithub /></a>
+              <a href="https://phatstraw.github.io/u-tube/" className="ghIcon"><FaLaptop /></a>
+              <a href="https://github.com/phatstraw/u-tube" className="ghIcon"><FaGithub /></a>
             </div>
           </div>
 
           <div className="item">
-            <h4>
-              JavaScript
-            </h4>
+            <div className='item-wrap'>
             <h2>
               Weather App
             </h2>
+            <img src={weather} />
+            </div>
+            
             <p>
               Sure we've got weather apps on our phones, but what about our desktops? Check the weather of a city in any country using this desktop weather app.
             </p>
             <div className="icons">
-              <a href="https://kevinsims1.github.io/weather-app/" className="ghIcon"><FaLaptop /></a>
-              <a href="https://github.com/kevinsims1/weather-app" className="ghIcon"><FaGithub /></a>
+              <a href="https://phatstraw.github.io/weather-app/" className="ghIcon"><FaLaptop /></a>
+              <a href="https://github.com/phatstraw/weather-app" className="ghIcon"><FaGithub /></a>
             </div>
           </div>
 
           <div className="item">
-            <h4>
-              JavaScript
-            </h4>
+            <div className='item-wrap'>
             <h2>
               Donor Manager
             </h2>
+            <img src={donor} />
+            </div>
+            
             <p>
               This app if for people who are apart of a non profit, and need a way to keep track of their donors and the amount they donated.
             </p>
             <div className="icons">
               <a href="https://donor-management-fe.netlify.com/signup" className="ghIcon"><FaLaptop /></a>
-              <a href="https://github.com/kevinsims1/OpenSource-marketplace-BE" className="ghIcon"><FaGithub /></a>
+              <a href="https://github.com/phatstraw/OpenSource-marketplace-BE" className="ghIcon"><FaGithub /></a>
             </div>
           </div>
         </div>
